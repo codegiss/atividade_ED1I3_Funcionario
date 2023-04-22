2f8adc213464f075bb54ea897bae26be9d232f84
codegiss <gisellesouza243@gmail.com>
2023-04-22 12:49:52 -0300

    funcionarios: incluir, excluir, pesquisar, listar

--- funcionario.cpp
+++ funcionario.cpp
@@ -14,11 +14,73 @@ Funcionario* init()
     return NULL;
 }
 
+Funcionario* insert(Funcionario* lista, int p, string n, double sal)
+{
+    Funcionario* novo = new Funcionario();
+    novo->prontuario = p;
+    novo->nome = n;
+    novo->salario = sal;
+    novo->prox = lista;
+    return novo;
+}
+
+Funcionario* remove(Funcionario* lista, int i)
+{
+    Funcionario* ant = NULL;
+    Funcionario* aux = lista;
+
+    while (aux != NULL && aux->prontuario != i)
+    {
+        ant = aux;
+        aux = aux->prox;
+    }
+    if (aux == NULL)
+    {
+        return lista;
+    }
+    if (ant == NULL)
+    {
+        lista = aux->prox;
+    }
+    else
+    {
+        ant->prox = aux->prox;
+    }
+    delete(aux);
+    return lista;
+}
+
+Funcionario* find(Funcionario* lista, int i)
+{
+    Funcionario* aux = lista;
+
+    while (aux != NULL && aux->prontuario != i)
+    {
+        aux = aux->prox;
+    }
+    return aux;
+}
+
+void print(Funcionario* lista)
+{
+    Funcionario* aux = lista;
+
+    while (aux != NULL)
+    {
+        cout << "Prontuario: " << aux->prontuario << endl;
+        cout << "Nome: " << aux->nome << endl;
+        cout << "Salario: " << aux->salario << endl;
+        aux = aux->prox;
+    }
+}
+
 int main(int argc, char** argv)
 {
-    int resp;
     Funcionario *lista;
     lista = init();
+    int resp, prontuario;
+    string nome;
+    double salario;
 
     do
     {
@@ -28,12 +90,12 @@ int main(int argc, char** argv)
         cout << "2. Excluir" << endl;
         cout << "3. Pesquisar" << endl;
         cout << "4. Listar" << endl;
-        cout << "Digite a opção desejada: ";
+        cout << "Digite a opcao desejada: ";
         cin >> resp;
 
         if (resp < 0 || resp > 4)
         {
-            cout << "Escolha uma opção válida de 0 a 4" << endl;
+            cout << "Escolha uma opcao valida de 0 a 4" << endl;
         }
     } while (resp < 0 || resp > 4);
 
@@ -41,19 +103,56 @@ int main(int argc, char** argv)
     {
         case 1:
             cout << endl;
-            cout << "INCLUIR FUNCIONÁRIO" << endl;
+            cout << "INCLUIR FUNCIONARIO" << endl;
+
+            cout << "Digite o prontuario: ";
+            cin >> prontuario;
+            cout << "Digite o nome: ";
+            cin >> nome;
+            cout << "Digite o salario: ";
+            cin >> salario;
+
+            lista = insert(lista, prontuario, nome, salario);
+
             break;
         case 2:
             cout << endl;
-            cout << "EXCLUIR FUNCIONÁRIO" << endl;
+            cout << "EXCLUIR FUNCIONARIO" << endl;
+
+            cout << "Digite o prontuario: ";
+            cin >> prontuario;
+
+            lista = remove(lista, prontuario);
+            cout << "Funcionario removido." << endl;
             break;
         case 3:
             cout << endl;
-            cout << "PESQUISAR FUNCIONÁRIO" << endl;
+            cout << "PESQUISAR FUNCIONARIO" << endl;
+
+            cout << "Digite o prontuario: ";
+            cin >> prontuario;
+
+            Funcionario *procura;
+            procura = find(lista, prontuario);
+
+            if(procura!=NULL)
+            {
+                cout << "Funcionario existente" << endl;
+                cout << "Prontuario: " << procura->prontuario << endl;
+                cout << "Nome: " << procura->nome << endl;
+                cout << "Salario: " << procura->salario << endl;
+            }
+            else
+            {
+                cout << "Funcionario inexistente" << endl;
+            }
+
             break;
         case 4:
             cout << endl;
-            cout << "LISTAR FUNCIONÁRIOS" << endl;
+            cout << "LISTAGEM DE FUNCIONARIOS" << endl;
+
+            print(lista);
             break;
         default:
             break;
